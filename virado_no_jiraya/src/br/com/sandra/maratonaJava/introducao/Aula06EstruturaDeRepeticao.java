506e25dc92de224034cc59b2387284400a42697a
sammy-regina <145820828+sammy-regina@users.noreply.github.com>
2025-10-16 18:57:01 -0300

    usando o contimnue

--- virado_no_jiraya/src/br/com/sandra/maratonaJava/introducao/Aula06EstruturaDeRepeticao.java
+++ virado_no_jiraya/src/br/com/sandra/maratonaJava/introducao/Aula06EstruturaDeRepeticao.java
@@ -2,7 +2,8 @@ package src.br.com.sandra.maratonaJava.introducao;
 
 public class Aula06EstruturaDeRepeticao {
     public static void main(String[] args) {
-        // whaile, do while, for
+            // Exemplo com while
+        System.out.println("\n--------------whaile--------------\n");
         int count = 0;
         while (count < 11) { // enquanto a condição < 10 for verdadeira, executa o bloco
             System.out.println("Count = " + count);
@@ -10,6 +11,7 @@ public class Aula06EstruturaDeRepeticao {
         }
 
         // Exemplo com do while
+        System.out.println("\n--------------do while--------------\n");
         count = 0;
         do {
             System.out.println("Count do while = " + count);
@@ -17,9 +19,20 @@ public class Aula06EstruturaDeRepeticao {
         } while (count < 13); // executa o bloco pelo menos uma vez, depois verifica a condição
 
         // Exemplo com for
+        System.out.println("\n-------------for---------------\n");
         for (int i = 0; i < 16; i++) { // emquanto i for menor que 16 incrementa 1
             
             System.out.println("Count for = " + i);
+        }
+
+        // Exemplo com continue
+        System.out.println("\n--------------continue--------------\n");
+        double valorTotal = 30000;
+        for (int parcela = (int) valorTotal; parcela >= 1; parcela--) {
+            if (valorTotal / parcela < 1000) {
+                continue; // pula para a próxima iteração se a condição for verdadeira
+            }
+            System.out.println("Parcela " + parcela + " R$ " + (valorTotal / parcela));
         }
 
     }
