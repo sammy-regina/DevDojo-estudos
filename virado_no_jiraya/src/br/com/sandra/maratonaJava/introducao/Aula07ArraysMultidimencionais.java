a2c4ddadcb0a7e5016c837dcfac448c26e20e01e
sammy-regina <145820828+sammy-regina@users.noreply.github.com>
2025-10-17 22:24:13 -0300

    usando arrays multidimencionais

---
+++ virado_no_jiraya/src/br/com/sandra/maratonaJava/introducao/Aula07ArraysMultidimencionais.java
@@ -0,0 +1,20 @@
+package src.br.com.sandra.maratonaJava.introducao;
+
+public class Aula07ArraysMultidimencionais {
+    public static void main(String[] args) {
+        int[][] dias = new int[3][3];
+        dias[0][0] = 31;
+        dias[0][1] = 28;
+        dias[0][2] = 31;
+
+        dias[1][0] = 30;
+        dias[1][1] = 31;
+        dias[1][2] = 30;
+
+        for (int i = 0; i < dias.length; i++) {
+            for (int j = 0; j < dias[0].length; j++) {
+                System.out.println(dias[i][j] + " ");
+            }
+        }
+    }
+}
