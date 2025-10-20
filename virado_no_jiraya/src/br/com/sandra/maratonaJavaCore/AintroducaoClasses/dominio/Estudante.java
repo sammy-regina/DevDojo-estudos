d7d5ec93e8bf527e368f75684ed0cf64afa18240
sammy-regina <145820828+sammy-regina@users.noreply.github.com>
2025-10-20 20:50:19 -0300

    classes

---
+++ virado_no_jiraya/src/br/com/sandra/maratonaJavaCore/AintroducaoClasses/dominio/Estudante.java
@@ -0,0 +1,7 @@
+package src.br.com.sandra.maratonaJavaCore.AintroducaoClasses.dominio;
+
+public class Estudante {
+    public String nome;
+    public int idade;
+    public char sexo;
+}
---
+++ virado_no_jiraya/test/EstudanteTest01.java
@@ -0,0 +1,13 @@
+package test;
+
+import src.br.com.sandra.maratonaJavaCore.AintroducaoClasses.dominio.Estudante;
+
+public class EstudanteTest01 {
+    public static void main(String[] args) {
+        Estudante estudante = new Estudante();
+        estudante.nome = "Sandra";
+        estudante.idade = 35;
+        estudante.sexo = 'F';
+        System.out.println("Nome: " + estudante.nome + "\nIdade: " + estudante.idade + "\nSexo: " + estudante.sexo);
+    }
+}
