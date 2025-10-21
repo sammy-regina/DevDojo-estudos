a23852cc3a37213cbd9fd1abc997cc027e2c0964
sammy-regina <145820828+sammy-regina@users.noreply.github.com>
2025-10-20 21:09:19 -0300

    valores padrao

--- virado_no_jiraya/src/br/com/sandra/maratonaJavaCore/AintroducaoClasses/dominio/Estudante.java
+++ virado_no_jiraya/src/br/com/sandra/maratonaJavaCore/AintroducaoClasses/dominio/Estudante.java
@@ -1,6 +1,7 @@
 package src.br.com.sandra.maratonaJavaCore.AintroducaoClasses.dominio;
 
 public class Estudante {
+    // se adicionar valor aqui, esse será o valor padrão
     public String nome;
     public int idade;
     public char sexo;
---
+++ virado_no_jiraya/test/EstudanteTest02.java
@@ -0,0 +1,15 @@
+package test;
+
+import src.br.com.sandra.maratonaJavaCore.AintroducaoClasses.dominio.Estudante;
+
+public class EstudanteTest02 {
+    public static void main(String[] args) {
+        Estudante estudante = new Estudante();
+
+        // Se nãoa tribuir o valor das variáveis, os valores padrões serão atribuídos(0, null, false)
+        System.out.println("Nome: " + estudante.nome);
+        System.out.println("Idade: " + estudante.idade);
+        System.out.println("Sexo: " + estudante.sexo);
+
+    }
+}
